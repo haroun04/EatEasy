99d984d121ad42df39fbbdd81bcf8043dfa1f87c
Haroun Zemrani <harounzemrani4@gmail.com>
2024-03-17 12:05:14 +0100

    Initial commit

---
+++ src/main/java/com/EatEasy/EatEasyApplication.java
@@ -0,0 +1,13 @@
+package com.EatEasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatEasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EatEasy/EatEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EatEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
