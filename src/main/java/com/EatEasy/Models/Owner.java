403a1fbc53cd564842add0a5952add95bcbf8a63
Mario Iruela <marioiruelaro@gmail.com>
2024-03-17 19:19:47 +0100

    Admin, Image and Owner created at Models

--- src/main/java/com/EatEasy/Models/Owner.java
+++ src/main/java/com/EatEasy/Models/Owner.java
@@ -1,12 +1,10 @@
 package com.EatEasy.Models;
-//El primer import se pone * para el @ID
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,24 +12,18 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Restaurant {
+public class Owner {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private UUID uuid;
     private String name;
-    private String location;
-    private String foodStyle;
-    private String timetable;
-    private Integer capacity;
-    private String phoneNumber;
+    private String email;
+    private String password;
 
     @OneToMany
-    private List<Image> images;
-
-    @OneToMany
-    private List<Booking> bookings;
-
+    private List<Restaurant> restaurants;
     @OneToMany
     private List<Review> reviews;
+
 }
