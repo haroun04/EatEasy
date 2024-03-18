d184a44479c8bc0278f8d4c463491bc7fe10742b
Mario Iruela <marioiruelaro@gmail.com>
2024-03-18 17:27:01 +0100

    Starting Mapper

---
+++ src/main/java/com/EatEasy/Mapper/AdminMapper.java
@@ -0,0 +1,49 @@
+package com.EatEasy.Mapper;
+
+import com.EatEasy.Models.Admin;
+import com.EatEasy.Dtos.AdminRequestDto;
+import com.EatEasy.Dtos.AdminResponseDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class AdminMapper {
+
+    public AdminResponseDto toResponse(Admin admin) {
+        return new AdminResponseDto(
+                admin.getId(),
+                admin.getUuid(),
+                admin.getName()
+        );
+    }
+
+    public List<AdminResponseDto> toResponse(List<Admin> admins) {
+        return admins.stream()
+                .map(this::toResponse)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+    }
+
+    public Admin toModel(AdminRequestDto adminRequestDto) {
+        return new Admin(
+                0L, // Asigna un valor válido para la base de datos
+                UUID.randomUUID(),
+                adminRequestDto.getName(),
+                adminRequestDto.getPassword()
+        );
+    }
+
+    public Admin toModel(Long adminId) {
+        return new Admin(
+                adminId,
+                null,
+                null,
+                null
+        );
+    }
+}
+
