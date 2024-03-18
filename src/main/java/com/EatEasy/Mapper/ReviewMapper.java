6a3cc393a42e1e6c2b70c0c2de21a6f0ab77483e
Mario Iruela <marioiruelaro@gmail.com>
2024-03-18 18:59:21 +0100

    MYSQL Running

--- src/main/java/com/EatEasy/Mapper/ReviewMapper.java
+++ src/main/java/com/EatEasy/Mapper/ReviewMapper.java
@@ -1,49 +1,53 @@
 package com.EatEasy.Mapper;
-import com.EatEasy.Models.Restaurant;
-import com.EatEasy.Models.Review;
 import com.EatEasy.Dtos.ReviewRequestDto;
 import com.EatEasy.Dtos.ReviewResponseDto;
+import com.EatEasy.Models.Review;
+import com.EatEasy.Models.Restaurant;
+import com.EatEasy.Models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
 
 @Component
 public class ReviewMapper {
+
     private final UserMapper userMapper;
     private final RestaurantMapper restaurantMapper;
 
-    //Constructor por defecto, autowired hay que ponerlo
     @Autowired
     public ReviewMapper(UserMapper userMapper, RestaurantMapper restaurantMapper) {
         this.userMapper = userMapper;
         this.restaurantMapper = restaurantMapper;
     }
 
-
-    public ReviewResponseDto toResponseDto(Review review) {
-        return new ReviewResponseDto(
-                review.getId(),
-                review.getUuid(),
-                review.getComment(),
-                review.getAssessment(),
-                review.getCreatedAt(),
-                review.getUser().getId(),
-                review.getRestaurant().getId()
-        );
-    }
-
-    public List<ReviewResponseDto> toResponseDtoList(List<Review> reviews) {
-        return reviews.stream().map(this::toResponseDto).collect(Collectors.toList());
+    public ReviewResponseDto toResponse(Review review) {
+        if (review != null) {
+            return new ReviewResponseDto(
+                    review.getId(),
+                    review.getUuid(),
+                    review.getComment(),
+                    review.getAssessment(),
+                    review.getCreatedAt(),
+                    review.getUser(),
+                    review.getRestaurant()
+            );
+        } else {
+            return null;
+        }
     }
 
     public Review toModel(ReviewRequestDto reviewRequestDto) {
-        Review review = new Review();
-        review.setComment(reviewRequestDto.getComment());
-        review.setAssessment(reviewRequestDto.getAssessment());
-        review.setCreatedAt(reviewRequestDto.getCreatedAt());
-        // Los IDs de usuario y restaurante se asignarán como referencias más adelante
-        return review;
+        return new Review(
+                0L,
+                UUID.randomUUID(),
+                reviewRequestDto.getComment(),
+                reviewRequestDto.getAssessment(),
+                LocalDateTime.now(),
+                null,
+                null
+        );
     }
 }
