9543dac04f06629bb4e009ddc94891748872cf5d
Haroun Zemrani <harounzemrani4@gmail.com>
2024-03-19 13:52:25 +0100

    faker

---
+++ src/main/java/com/EatEasy/Services/InitialDataCreationService.java
@@ -0,0 +1,13 @@
+package com.EatEasy.Services;
+
+import com.EatEasy.Models.Booking;
+
+public class InitialDataCreationService {
+    private final AdminService adminService;
+    private final BookingService bookingService;
+    private final FavoriteRestaurantService favoriteRestaurantService;
+    private final ImageService imageService;
+    private final OwnerService ownerService;
+    private final RestaurantService restaurantService;
+    private final UserService userService;
+}
