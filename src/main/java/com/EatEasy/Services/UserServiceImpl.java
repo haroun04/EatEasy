0f41a6024a8587b5f1812560e408f0e7f8ec0fe7
Haroun04 <Harounzemrani4@gmail.com>
2024-04-18 20:18:00 +0200

    estrella

--- src/main/java/com/EatEasy/Services/UserServiceImpl.java
+++ src/main/java/com/EatEasy/Services/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.EatEasy.Services;
 import com.EatEasy.Models.User;
 import com.EatEasy.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,7 +15,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(serRepository userRepository) {
         this.userRepository = userRepository;
     }
 
